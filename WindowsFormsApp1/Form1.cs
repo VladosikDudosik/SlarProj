36062c98d4b3d12e1a6af0b69ce4dae2ab3dae11
vladdutcha <dutchakpsix@gmail.com>
2022-04-14 00:41:27 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLibrary.Mathematics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        double[,] matrix = new double[3,3];
+        double[] x = new double[3];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void onlyNumber(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!(Char.IsDigit(number) || Char.IsControl(number) || Char.IsPunctuation(number)))
+            {
+                e.Handled = true;
+            }
+        }
+        private void button_calculate_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex != -1)
+            {
+                try
+                {
+                    matrix = new double[3, 3]
+                    {
+                    {double.Parse(textBox1.Text),double.Parse(textBox2.Text),double.Parse(textBox3.Text) },
+                    {double.Parse(textBox4.Text),double.Parse(textBox5.Text),double.Parse(textBox6.Text) },
+                    {double.Parse(textBox7.Text),double.Parse(textBox8.Text),double.Parse(textBox9.Text) }
+                    };
+                    x = new double[3] { double.Parse(textBox10.Text), double.Parse(textBox11.Text), double.Parse(textBox12.Text) };
+                    
+                    double[] answers = new double[3];
+
+                    if (comboBox1.SelectedIndex == 0)
+                    {
+                        try
+                        {
+                            answers = SystemOfLinearAlgebraicEquations.GaussianMethod(matrix, x);
+                        }
+                        catch (ArgumentException)
+                        {
+                            MessageBox.Show("Неможливо обчислити методом Гаусса", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            answers = SystemOfLinearAlgebraicEquations.MethodOfSimpleIteration(matrix, x);
+                        }
+                        catch (ArgumentException)
+                        {
+                            MessageBox.Show("Ітераційний процес не збігається - дану СЛАР неможливо обрахувати методом простих ітерацій", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        
+                    }
+                    
+                    textBox_res1.Text = answers[0].ToString();
+                    textBox_res2.Text = answers[1].ToString();
+                    textBox_res3.Text = answers[2].ToString();
+                }
+                catch
+                {
+                    MessageBox.Show("Можливо деякі комірки пусті, або містять у собі некоректне значення", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Оберіть метод", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Models/SimultaneousLinearEquations.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DVLibrary.Mathematics
+{
+    public static class SystemOfLinearAlgebraicEquations
+    {
+        public static double[] GaussianMethod(double[,] a,double[] b,double eps = 0.001)
+        {
+            int n = b.Length;
+            double[] x = b;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (Math.Abs(a[i, i]) < eps)
+                {
+                    throw new ArgumentException();
+                }
+                for (int j = i + 1; j < n; j++)
+                {
+                    a[j, i] = -a[j, i] / a[i, i];
+                    for (int k = i + 1; k < n; k++)
+                    {
+                        a[j, k] += a[j, i] * a[i, k];
+                    }
+                    b[j] += a[j, i] * b[i];
+                }
+            }
+            x[n - 1] = b[n - 1] / a[n - 1, n - 1];
+            double h = 0;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                h = b[i];
+                for (int j = i + 1; j < n; j++)
+                {
+                    h -= x[j] * a[i, j];
+                }
+                x[i] = h / a[i, i];
+            }
+            return x;
+        }
+        public static double[] MethodOfSimpleIteration(double[,] a, double[] b, double eps = 0.001)
+        {
+            int n = b.Length;
+            double[] res = new double[n], Xn = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                double sum = 0;
+                res[i] = b[i] / a[i, i];
+                for (int j = i + 1; j < n; j++) sum += Math.Abs(a[i, j]);
+                if (Math.Abs(a[i, i]) <= sum) throw new ArgumentException();
+            }
+
+            do
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Xn[i] = b[i] / a[i, i];
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i != j) Xn[i] -= a[i, j] / a[i, i] * res[j];
+                    }
+                }
+
+                bool flag = true;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (Math.Abs(Xn[i] - res[i]) > eps)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    res[i] = Xn[i];
+                }
+                if (flag)
+                    break;
+            } while (true);
+
+            return res;
+        }
+    }
+}
+
